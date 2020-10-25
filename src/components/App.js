2cc2abb3668462f513a42b3e2ec7f74c7cdf650b
Preeti Sahani <43055616+preetisahani21@users.noreply.github.com>
2020-10-25 21:46:49 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect } from "react";
+import React, { Component, useState, useEffect, useCallback } from "react";
 import "../styles/App.css";
 
 const App = () => {
@@ -10,6 +10,7 @@ const App = () => {
     top: "0px",
     position: "absolute"
   });
+  //const [style, setStyle] = useState(ballPosition);
   const reset = () => {
     setRenderBall(false);
     setX(0);
@@ -22,29 +23,66 @@ const App = () => {
   };
   const start = () => {
     setRenderBall(true);
+    //document.addEventListener("keydown", handlekey);
   };
-  useEffect(() => {
-    document.addEventListener("keydown", handlekey);
+  /*useEffect(() => {
+    //document.addEventListener("keydown", handlekey);
   });
   const handlekey = (event) => {
     console.log(event.key);
     if (event.key === "ArrowRight") {
       let k = x + 5;
       setX(k);
+      setBallPosition({ left: `${x}px`, top: `${y}px`, position: `absolute` });
       console.log(x);
     } else if (event.key === "ArrowLeft") {
       const k = x - 5;
       setX(k);
+      setBallPosition({ left: `${x}px`, top: `${y}px`, position: `absolute` });
     } else if (event.key === "ArrowUp") {
       const k = y - 5;
       setY(k);
+      setBallPosition({ left: `${x}px`, top: `${y}px`, position: `absolute` });
     } else if (event.key === "ArrowDown") {
       const k = y + 5;
       setY(k);
+      setBallPosition({ left: `${x}px`, top: `${y}px`, position: `absolute` });
     }
-    setBallPosition({ left: `${x}px`, top: `${y}px`, position: `absolute` });
     console.log(x + " " + y);
-  };
+    //setBallPosition({ left: `${x}px`, top: `${y}px`, position: `absolute` });
+    setStyle(ballPosition);
+  };*/
+  const handleUserKeyPress = useCallback(
+    (event) => {
+      console.log(event.key);
+      if (event.key === "ArrowRight") {
+        let k = x + 5;
+        setX(k);
+        console.log(x);
+      } else if (event.key === "ArrowLeft") {
+        const k = x - 5;
+        setX(k);
+      } else if (event.key === "ArrowUp") {
+        const k = y - 5;
+        setY(k);
+      } else if (event.key === "ArrowDown") {
+        const k = y + 5;
+        setY(k);
+      }
+      console.log(x + " " + y);
+      setBallPosition({ left: `${x}px`, top: `${y}px`, position: `absolute` });
+      //setStyle(ballPosition);
+    },
+    [x, y]
+  );
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleUserKeyPress);
+
+    return () => {
+      window.removeEventListener("keydown", handleUserKeyPress);
+    };
+  }, [handleUserKeyPress, x, y]);
 
   const renderChoice = () => {
     if (!renderBall) {
